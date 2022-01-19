d6a283d6762a8e4da3e1d2115f559eef1e397851
mmednik <mmednik@gmail.com>
2022-01-19 11:51:03 -0300

    formatting

--- index.js
+++ index.js
@@ -1,98 +1,99 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "hello there! Welcome to my new telegram bot.",
+    {}
+  );
+});
 
-bot.command('start', ctx => {
-  console.log(ctx.from)
-  bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
-  })
-})
-
-
-bot.hears('animals', ctx => {
-  console.log(ctx.from)
+bot.hears("animals", (ctx) => {
+  console.log(ctx.from);
   let animalMessage = `great, here are pictures of animals you would love`;
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
-      reply_markup: {
-          inline_keyboard: [
-              [{
-                      text: "dog",
-                      callback_data: 'dog'
-                  },
-                  {
-                      text: "cat",
-                      callback_data: 'cat'
-                  }
-              ],
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "dog",
+            callback_data: "dog",
+          },
+          {
+            text: "cat",
+            callback_data: "cat",
+          },
+        ],
+      ],
+    },
+  });
+});
 
-          ]
-      }
-  })
-})
-
-bot.action('dog', ctx => {
+bot.action("dog", (ctx) => {
   bot.telegram.sendPhoto(ctx.chat.id, {
-      source: "res/dog.jpg"
-  })
+    source: "res/dog.jpg",
+  });
+});
 
-})
-
-//method that returns image of a cat 
-
-bot.action('cat', ctx => {
+bot.action("cat", (ctx) => {
   bot.telegram.sendPhoto(ctx.chat.id, {
-      source: "res/cat.jpg"
-  })
+    source: "res/cat.jpg",
+  });
+});
 
-})
-
-
-bot.hears('phone', (ctx, next) => {
-  console.log(ctx.from)
-  bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
-
-})
-
-//method for requesting user's location
+bot.hears("phone", (ctx, next) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Can we get access to your phone number?",
+    requestPhoneKeyboard
+  );
+});
 
 bot.hears("location", (ctx) => {
-  console.log(ctx.from)
-  bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
-})
-
-//constructor for providing phone number to the bot
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Can we access your location?",
+    requestLocationKeyboard
+  );
+});
 
 const requestPhoneKeyboard = {
-  "reply_markup": {
-      "one_time_keyboard": true,
-      "keyboard": [
-          [{
-              text: "My phone number",
-              request_contact: true,
-              one_time_keyboard: true
-          }],
-          ["Cancel"]
-      ]
-  }
+  reply_markup: {
+    one_time_keyboard: true,
+    keyboard: [
+      [
+        {
+          text: "My phone number",
+          request_contact: true,
+          one_time_keyboard: true,
+        },
+      ],
+      ["Cancel"],
+    ],
+  },
 };
-//constructor for proving location to the bot
 
 const requestLocationKeyboard = {
-  "reply_markup": {
-      "one_time_keyboard": true,
-      "keyboard": [
-          [{
-              text: "My location",
-              request_location: true,
-              one_time_keyboard: true
-          }],
-          ["Cancel"]
-      ]
-  }
-
-}
+  reply_markup: {
+    one_time_keyboard: true,
+    keyboard: [
+      [
+        {
+          text: "My location",
+          request_location: true,
+          one_time_keyboard: true,
+        },
+      ],
+      ["Cancel"],
+    ],
+  },
+};
 
 bot.launch();
