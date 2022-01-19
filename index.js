662af7f9e14e6f4a3a46513df640050fc71ec5ce
mmednik <mmednik@gmail.com>
2022-01-19 09:52:29 -0300

    base bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5000452490:AAETQe5XQfBQAGETNdnoT8lrr5iUB53CHOY';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Received your message');
+});
